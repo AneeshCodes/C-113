b6abf8006e70acef82d731d09ec449da0efa5bd6
AneeshCodes <74843895+AneeshCodes@users.noreply.github.com>
2021-09-20 17:47:18 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+tint_color = ""
+y = 10
+
+function preload()
+{
+  
+}
+
+function setup()
+{
+  canvas = createCanvas(400, 400);
+  canvas.center()
+  video = createCapture(VIDEO)
+  video.hide();
+  
+}
+
+function draw()
+{
+  image(video, 0, 0, 400, 400)
+  tint(tint_color)
+  circle(10,10,20)
+  circle(10,390,20)
+  circle(390,10,20)
+  circle(390,390,20);
+  
+}
+
+function applyFilter()
+{
+  tint_color = document.getElementById('filter_color').value
+}
+
+function take_snapshot()
+{
+  save("image.png")
+}
